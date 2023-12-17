2f475ec9d6c83b932302e361c01761a82de898d0
wan <wanhuz96@gmail.com>
2023-12-17 08:23:40 +0800

    Small refactoring

--- module/util.py
+++ module/util.py
@@ -1,3 +1,5 @@
+from os import listdir, remove
+
 class Util:
     def clean_drag_and_drop_quote(fname : str) -> str:
         if ("'" in fname[0]):
@@ -13,3 +15,9 @@ class Util:
         if ('.zip' in zip_path):
             return True
         return False
+    
+    def remove_avif_file_in_directory(path : str) -> None:
+        for filename in listdir(path):
+            filepath = path + '/' + filename
+            if ('.avif' in filename):
+                remove(filepath)
--- start.py
+++ start.py
@@ -10,6 +10,7 @@ def main():
     zip_path = Util.clean_drag_and_drop_quote(zip_path)
     is_given_dir = False
 
+
     if (Util.is_zip(zip_path)):
         print('Unzipping ' + zip_path + '...')
         unzip_path = Zip.unzip(zip_path)
@@ -20,21 +21,23 @@ def main():
     else:
         print('Not a valid zip file or directory')
         input('Press any key to exit.')
+        exit()
+
 
     print('Converting to JPG...')
     for filename in os.listdir(unzip_path):
         filepath = unzip_path + '/' + filename
         Converter.convert_to_jpg(filepath)
 
+
     print('Removing temporary file...')
-    for filename in os.listdir(unzip_path):
-        filepath = unzip_path + '/' + filename
-        if ('avif' in filename):
-            os.remove(filepath)
+    Util.remove_avif_file_in_directory(unzip_path)
+
 
     print('CBZipping file...')
     cbzed_file = Zip.cbzip(unzip_path)
     shutil.rmtree(unzip_path)
+
 
     if (is_given_dir):
         new_cbz_name = cbzed_file.replace('_temp', '')
